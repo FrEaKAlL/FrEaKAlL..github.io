fda163fa3934735ceabf4855583ecaa7b4c5416c
Sergio Calalpa <sercal0121@gmail.com>
2023-04-18 02:04:34 -0600

    Galeraia
    Se agrega menejo correcto de Galeria

--- content/js/static.js
+++ content/js/static.js
@@ -4,3 +4,11 @@
     let contenedorHeader = this.document.querySelector('.cont-menu-principal');
     contenedorHeader.classList.toggle('cambia-cont', window.scrollY > 630);
 }); */
+function abriImagen(url, alt) {
+    $('#fill-img-pant').attr('src', url);
+    $('#fill-img-pant').attr('alt', alt);
+    $('.fill-img').removeClass('oculto');
+}
+function cierraImg() {
+    $('.fill-img').addClass('oculto');
+}
